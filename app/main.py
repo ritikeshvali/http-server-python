5ab9599d86785c98e5c500fdd8550b47eabc3f44
Ritikesh Vali <ritikeshvali2001@gmail.com>
2024-03-28 05:10:43 +0530

    Respond with 404

--- app/main.py
+++ app/main.py
@@ -2,20 +2,27 @@
 # import socket
 import socket
 
+def http_response(conn, addr):
+    data: bytes = conn.recv(1024).decode()
+    lines = data.split("\r\n")
+
+    path = lines[0].split(" ")[1]
+
+    if path == '/':
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    conn.close()
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
     client_socket, client_address = server_socket.accept()
     print(f"Received a connection from {client_address}")
-    client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    http_response(client_socket, client_address)
 
 
 if __name__ == "__main__":
